0b2990021a932c7c444884c2e638934c0dc13853
Alexia Cardoso <alexiacardoso@suasfacil.com>
2024-12-12 11:04:20 -0300

    adiciona redirecionamentos de rota

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,1 +1,16 @@
 <h3>Principal (view)</h3>
+
+<ul>
+  <li>
+    <a href="{{ route('site.index') }}">Principal</a>
+  </li>
+  <li>
+    <a href="{{ route('site.sobreNos') }}">Sobre Nós</a>
+  </li>
+  <li>
+    <a href="{{ route('site.contato') }}">Contato</a>
+  </li>
+  <li>
+    <a href="{{ route('site.rota2') }}">Rota 2 para rota 1</a>
+  </li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,35 @@ use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\ContatoController;
 
-Route::get('/', [PrincipalController::class, 'principal']);
-Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
-Route::get('/contato', [ContatoController::class, 'contato']);
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobreNos');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function () {
+  return 'login';
+})->name('site.login');
+
+Route::get('/rota1', function () {
+  return 'Rota 1';
+})->name('site.rota1');
+
+Route::get('/rota2', function () {
+  return redirect()->route('site.rota1');
+})->name('site.rota2');
+// Route::redirect('/rota2', 'rota1');
+
+Route::prefix('/app')->group(function () {
+  Route::get('/clientes', function () {
+    return 'clientes';
+  })->name('app.clientes');
+  Route::get('/fornecedores', function () {
+    return 'fornecedores';
+  })->name('app.fornecedores');
+  ;
+  Route::get('/produtos', function () {
+    return 'produtos';
+  })->name('app.produtos');
+});
+
 
 // passando parametros
 Route::get('/contato/{id}', function (string $id) {
