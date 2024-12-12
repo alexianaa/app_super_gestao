a879f885dbf690c39fa5392c28beea9dbb7eae99
Alexia Cardoso <alexiacardoso@suasfacil.com>
2024-12-12 10:35:11 -0300

    adiciona rotas com parametros

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,18 @@ use App\Http\Controllers\ContatoController;
 Route::get('/', [PrincipalController::class, 'principal']);
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
 Route::get('/contato', [ContatoController::class, 'contato']);
+
+// passando parametros
+Route::get('/contato/{id}', function (string $id) {
+  return "User $id";
+})->whereNumber('id');
+
+// parametros opcionais
+Route::get('/user/{name?}', function (?string $name = null) {
+  return $name;
+})->whereAlpha('name');
+
+// expressoes regulares
+Route::get('/number/{id?}', function (int $id) {
+  return "numero = $id";
+})->where('id', '[0-9]+');
