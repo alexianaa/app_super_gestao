0656828404803edaa3dd2442a9b8f437d63467fc
Alexia Cardoso <alexiacardoso@suasfacil.com>
2025-01-31 10:51:09 -0300

    adiciona uso do old na pagina principal

--- app/Http/Controllers/PrincipalController.php
+++ app/Http/Controllers/PrincipalController.php
@@ -8,6 +8,12 @@ class PrincipalController extends Controller
 {
     public function principal()
     {
-        return view('site/principal');
+        $motivos_contato = [
+            1 => 'Dúvida',
+            2 => 'Elogio',
+            3 => 'Reclamação'
+        ];
+
+        return view('site.principal', ['motivos_contato' => $motivos_contato]);
     }
 }
