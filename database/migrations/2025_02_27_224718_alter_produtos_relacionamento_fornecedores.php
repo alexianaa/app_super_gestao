f0969c64e53d390d0c068f4a143e06d1f8f2e595
Alexia Cardoso <alexiacardoso@suasfacil.com>
2025-02-27 20:23:33 -0300

    migration

---
+++ database/migrations/2025_02_27_224718_alter_produtos_relacionamento_fornecedores.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('produtos', function (Blueprint $table) {
+
+            $fornecedorId = DB::table('fornecedores')->insertGetId([
+                'nome' => 'Fornecedor Padrao',
+                'site' => 'fornecedorpadrao.com.br',
+                'uf' => 'SP',
+                'email' => 'fornecedorpadrao@example.com',
+            ]);
+
+            // $table->unsignedBigInteger('fornecedor_id')->default($fornecedorId);
+            // $table->foreign('fornecedor_id')->references('id')->on('fornecedores')->onDelete('restrict')->onUpdate('restrict');
+
+            $table->foreignId('fornecedor_id')->default($fornecedorId)->constrained(table: 'fornecedores',indexName: 'produto_fornecedor_id')->onDelete('restrict')->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->dropColumn('fornecedor_id');
+        });
+    }
+};
